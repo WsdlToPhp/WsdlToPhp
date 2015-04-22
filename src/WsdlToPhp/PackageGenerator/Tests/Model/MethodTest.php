4de4fa27ed5612f261d9e394165c0efa1a2c8ca8
Mikael DELSOL <delsol.mikael@gmail.com>
2015-04-22 16:59:54 +0200

    add unit test for Method class

---
+++ src/WsdlToPhp/PackageGenerator/Tests/Model/MethodTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace WsdlToPhp\PackageGenerator\Tests;
+
+use WsdlToPhp\PackageGenerator\Model\Service;
+use WsdlToPhp\PackageGenerator\Model\Method;
+use WsdlToPhp\PackageGenerator\Tests\TestCase;
+
+class MethodTest extends TestCase
+{
+    /**
+     *
+     */
+    public function testGetMethodName()
+    {
+        $service = new Service('Foo');
+        $service->addMethod('getId', 'string', 'string');
+        $service->addMethod('getid', 'string', 'string');
+        $service->addMethod('getIdString', 'string', 'id', false);
+        $service->addMethod('getIdInt', 'int', 'id', false);
+
+        $this->assertEquals('getId', $service->getMethod('getId')->getMethodName());
+        $this->assertEquals('getid_1', $service->getMethod('getid')->getMethodName());
+        $this->assertEquals('getIdStringString', $service->getMethod('getIdString')->getMethodName());
+        $this->assertEquals('getIdIntInt', $service->getMethod('getIdInt')->getMethodName());
+    }
+}
