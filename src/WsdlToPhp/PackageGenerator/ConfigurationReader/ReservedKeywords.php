1766c76c4aa54a8d5b8c56aa98135b6481515b62
Mikael DELSOL <delsol.mikael@gmail.com>
2015-04-22 10:31:43 +0200

    add Reader to reserved keywords configuration file

---
+++ src/WsdlToPhp/PackageGenerator/ConfigurationReader/ReservedKeywords.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace WsdlToPhp\PackageGenerator\ConfigurationReader;
+
+class ReservedKeywords extends AbstractYamlReader
+{
+    /**
+     * List of PHP reserved keywords from config file
+     * @var array
+     */
+    protected $keywords;
+    /**
+     * @param string $filename
+     */
+    protected function __construct($filename)
+    {
+        $this->keywords = array();
+        $this->parseReservedKeywords($filename);
+    }
+    /**
+     * @param string $filename
+     */
+    protected function parseReservedKeywords($filename)
+    {
+        $keywords = $this->loadYaml($filename);
+        if (!isset($keywords['reserved_keywords'])) {
+            throw new \InvalidArgumentException(sprintf('Unable to find section reserved_keywords in "%s"', $filename));
+        }
+        $this->keywords = $keywords['reserved_keywords'];
+    }
+    /**
+     * @param string options's file to parse
+     * @return ReservedKeywords
+     */
+    public static function instance($filename = null)
+    {
+        return parent::instance(empty($filename) ? dirname(__FILE__) . '/../Resources/config/reserved_keywords.yml' : $filename);
+    }
+    /**
+     * @return bool
+     */
+    public function is($keyword)
+    {
+        return in_array($keyword, $this->keywords, true);
+    }
+}
