9102ca1b01c6a0c1026aee1fe243ab48d30598e2
Mikael DELSOL <delsol.mikael@gmail.com>
2015-04-22 16:25:04 +0200

    add unit test class for Struct class

---
+++ src/WsdlToPhp/PackageGenerator/Tests/Model/StructTest.php
@@ -0,0 +1,103 @@
+<?php
+namespace WsdlToPhp\PackageGenerator\Tests;
+
+use WsdlToPhp\PackageGenerator\Model\StructValue;
+
+use WsdlToPhp\PackageGenerator\Model\Struct;
+use WsdlToPhp\PackageGenerator\Tests\TestCase;
+
+class StructTest extends TestCase
+{
+    public static function instance($name, $isStruct)
+    {
+        return new Struct($name, $isStruct);
+    }
+    /**
+     *
+     */
+    public function testGetContextualPartEnum()
+    {
+        $struct = self::instance('Foo', false);
+        $struct->setIsRestriction(true);
+        $this->assertEquals(Struct::CONTEXTUAL_PART_ENUMERATION, $struct->getContextualPart());
+        $struct->setIsStruct(false);
+        $this->assertEquals(Struct::CONTEXTUAL_PART_ENUMERATION, $struct->getContextualPart());
+        $struct->setIsStruct(true);
+        $this->assertEquals(Struct::CONTEXTUAL_PART_ENUMERATION, $struct->getContextualPart());
+    }
+    /**
+     *
+     */
+    public function testGetDocSubPackagesEnum()
+    {
+        $struct = self::instance('Foo', false);
+        $struct->setIsRestriction(true);
+        $this->assertContains(Struct::DOC_SUB_PACKAGE_ENUMERATIONS, $struct->getDocSubPackages());
+        $struct->setIsStruct(false);
+        $this->assertContains(Struct::DOC_SUB_PACKAGE_ENUMERATIONS, $struct->getDocSubPackages());
+        $struct->setIsStruct(true);
+        $this->assertContains(Struct::DOC_SUB_PACKAGE_ENUMERATIONS, $struct->getDocSubPackages());
+    }
+    /**
+     *
+     */
+    public function testGetContextualPartStruct()
+    {
+        $struct = self::instance('Foo', false);
+        $this->assertEquals(Struct::CONTEXTUAL_PART_STRUCT, $struct->getContextualPart());
+        $struct->setIsRestriction(false);
+        $this->assertEquals(Struct::CONTEXTUAL_PART_STRUCT, $struct->getContextualPart());
+        $struct->setIsStruct(true);
+        $this->assertEquals(Struct::CONTEXTUAL_PART_STRUCT, $struct->getContextualPart());
+    }
+    /**
+     *
+     */
+    public function testGetDocSubPackagesStruct()
+    {
+        $struct = self::instance('Foo', false);
+        $this->assertContains(Struct::DOC_SUB_PACKAGE_STRUCTS, $struct->getDocSubPackages());
+        $struct->setIsRestriction(false);
+        $this->assertContains(Struct::DOC_SUB_PACKAGE_STRUCTS, $struct->getDocSubPackages());
+    }
+    /**
+     *
+     */
+    public function testIsArray()
+    {
+        $struct = self::instance('ArrayFoo', false);
+        $struct->addAttribute('ArrayOfId', 'array');
+        $this->assertTrue($struct->isArray());
+
+        $struct->setName('Foo');
+        $this->assertFalse($struct->isArray());
+
+        $struct->setName('ArrayFoo');
+        $struct->addAttribute('ArrayOfString', 'array');
+        $this->assertFalse($struct->isArray());
+    }
+    /**
+     *
+     */
+    public function testGetValue()
+    {
+        $struct = self::instance('Foo', true);
+        $struct->addValue('id');
+        $struct->addValue('name');
+        $struct->addValue('_key');
+        $this->assertInstanceOf('\\WsdlToPhp\\PackageGenerator\\Model\\StructValue', $struct->getValue('id'));
+        $this->assertNotInstanceOf('\\WsdlToPhp\\PackageGenerator\\Model\\StructValue', $struct->getValue('_id'));
+    }
+    /**
+     *
+     */
+    public function testGetAttibute()
+    {
+        $struct = self::instance('Foo', true);
+        $struct->addAttribute('id', 'int');
+        $struct->addAttribute('name', 'string');
+        $struct->addAttribute('_key', 'string');
+        $this->assertInstanceOf('\\WsdlToPhp\\PackageGenerator\\Model\\StructAttribute', $struct->getAttribute('id'));
+        $this->assertNotInstanceOf('\\WsdlToPhp\\PackageGenerator\\Model\\StructAttribute', $struct->getAttribute('_id'));
+    }
+}
