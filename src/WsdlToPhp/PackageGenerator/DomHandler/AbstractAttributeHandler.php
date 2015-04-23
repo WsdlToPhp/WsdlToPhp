852086759b49c41137282fb55dbf3e9b88df349b
Mikael DELSOL <delsol.mikael@gmail.com>
2015-04-23 19:44:47 +0200

    update attributehandler class

--- src/WsdlToPhp/PackageGenerator/DomHandler/AbstractAttributeHandler.php
+++ src/WsdlToPhp/PackageGenerator/DomHandler/AbstractAttributeHandler.php
@@ -2,24 +2,50 @@
 
 namespace WsdlToPhp\PackageGenerator\DomHandler;
 
-class AbstractAttributeHandler
+use WsdlToPhp\PackageGenerator\Generator\Utils;
+
+class AbstractAttributeHandler extends AbstractNodeHandler
 {
     /**
-     * @var string
+     * @see \WsdlToPhp\PackageGenerator\DomHandler\AbstractNodeHandler::getNode()
+     * @return \DOMAttr
      */
-    protected $name;
-    /**
-     * @var mixed
-     */
-    protected $value;
-    /**
-     * @param string $name
-     * @param string $value
-     * @return AbstractAttributeHandler
-     */
-    public function __construct($name, $value)
+    public function getNode()
     {
-        $this->name  = $name;
-        $this->value = $value;
+        return parent::getNode();
+    }
+    /**
+     * @return \DOMAttr
+     */
+    public function getAttribute()
+    {
+        return $this->getNode();
+    }
+    /**
+     * Tries to get attribute type on the same node
+     * in order to return the value of the attribute in its type
+     */
+    public function getType()
+    {
+        $type = null;
+        if ($this->getParent() instanceof ElementHandler && $this->getParent()->hasAttribute('type')) {
+            $type = $this->getParent()->getAttribute('type');
+        }
+        return $type;
+    }
+    /**
+     * @param bool $withNamespace
+     * @return string
+     */
+    public function getValue($withNamespace = false, $withinItsType = true)
+    {
+        $value = $this->getAttribute()->value;
+        if ($withNamespace === false && !empty($value)) {
+            $value = implode('', array_slice(explode(':', $value), -1, 1));
+        }
+        if ($value !== null && $withinItsType === true) {
+            $value = Utils::getValueWithinItsType($value, $this->getType());
+        }
+        return $value;
     }
 }
