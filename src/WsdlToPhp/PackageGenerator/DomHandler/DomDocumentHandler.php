7d794431c3e671c263525968fe42251950df3579
Mikael DELSOL <delsol.mikael@gmail.com>
2015-04-23 19:45:50 +0200

    add getAttributeHandler method

--- src/WsdlToPhp/PackageGenerator/DomHandler/DomDocumentHandler.php
+++ src/WsdlToPhp/PackageGenerator/DomHandler/DomDocumentHandler.php
@@ -13,13 +13,22 @@ class DomDocumentHandler extends AbstractDomDocumentHandler
     protected function getNodeHandler(\DOMNode $node, AbstractDomDocumentHandler $domDocument, $index = -1)
     {
         return new NodeHandler($node, $domDocument, $index);
-    }/**
+    }
+    /**
      * @see \WsdlToPhp\PackageGenerator\DomHandler\AbstractDomDocumentHandler::getElementHandler()
      * @return ElementHandler
      */
     protected function getElementHandler(\DOMElement $element, AbstractDomDocumentHandler $domDocument, $index = -1)
     {
         return new ElementHandler($element, $domDocument, $index);
+    }
+    /**
+     * @see \WsdlToPhp\PackageGenerator\DomHandler\AbstractDomDocumentHandler::getAttributeHandler()
+     * @return AttributeHandler
+     */
+    protected function getAttributeHandler(\DOMAttr $attribute, AbstractDomDocumentHandler $domDocument, $index = -1)
+    {
+        return new AttributeHandler($attribute, $domDocument, $index);
     }
     /**
      * @return NodeHandler
