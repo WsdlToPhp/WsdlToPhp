9974220e92eb488d5a64b7a339ccac107a058ece
Mikael DELSOL <delsol.mikael@gmail.com>
2015-04-23 19:12:03 +0200

    remove useless use statement, remove unused method

--- src/WsdlToPhp/PackageGenerator/Tests/DomHandler/NodeHandlerTest.php
+++ src/WsdlToPhp/PackageGenerator/Tests/DomHandler/NodeHandlerTest.php
@@ -3,19 +3,13 @@
 namespace WsdlToPhp\PackageGenerator\Tests\DomHandler;
 
 use WsdlToPhp\PackageGenerator\Tests\TestCase;
-use WsdlToPhp\PackageGenerator\DomHandler\AbstractNodeHandler;
 
 class NodeHandlerTest extends TestCase
 {
-    public static function instance()
-    {
-        return new NodeHandler();
-    }
     /**
      *
      */
     public function testGetName()
     {
-
     }
 }
