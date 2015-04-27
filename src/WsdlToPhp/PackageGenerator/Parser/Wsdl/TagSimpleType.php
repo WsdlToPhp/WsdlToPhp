1a3167b16d2e2910409dc2a6247ed03ea845b62b
Mikael DELSOL <delsol.mikael@gmail.com>
2015-04-27 23:18:48 +0200

    add SimpleType parser

---
+++ src/WsdlToPhp/PackageGenerator/Parser/Wsdl/TagSimpleType.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace WsdlToPhp\PackageGenerator\Parser\Wsdl;
+
+use WsdlToPhp\PackageGenerator\Model\Wsdl;
+
+class TagSimpleType extends AbstractParser
+{
+	/**
+     * @see \WsdlToPhp\PackageGenerator\Parser\Wsdl\AbstractParser::parseWsdl()
+     */
+    protected function parseWsdl(Wsdl $wsdl)
+    {
+    }
+	/**
+     * @see \WsdlToPhp\PackageGenerator\Parser\Wsdl\AbstractParser::parsingTag()
+     */
+    protected function parsingTag()
+    {
+    }
+}
