e8caecfeb9fbd1110f2d9b1b9fd2dd6901a35784
Mikael DELSOL <delsol.mikael@gmail.com>
2015-04-27 10:02:33 +0200

    enhance comments and methods, add getStructByName metho, change addStruct method scope

--- src/WsdlToPhp/PackageGenerator/ModelContainer/StructContainer.php
+++ src/WsdlToPhp/PackageGenerator/ModelContainer/StructContainer.php
@@ -6,9 +6,21 @@ use WsdlToPhp\PackageGenerator\Model\Struct;
 
 class StructContainer extends ModelContainer
 {
+    /**
+     * @see \WsdlToPhp\PackageGenerator\ModelContainer\ModelContainer::modelClass()
+     * @return string
+     */
     protected function modelClass()
     {
         return 'WsdlToPhp\\PackageGenerator\\Model\\Struct';
+    }
+    /**
+     * @param string $name
+     * @return Struct|null
+     */
+    public function getStructByName($name)
+    {
+        return $this->get($name, parent::KEY_NAME);
     }
     /**
      * Adds a virtual struct
@@ -29,7 +41,7 @@ class StructContainer extends ModelContainer
      * @param string $attributeType the attribute type
      * @return StructContainer
      */
-    private function addStruct($structName, $attributeName, $attributeType)
+    public function addStruct($structName, $attributeName, $attributeType)
     {
         if ($this->get($structName) === null) {
             $this->add(new Struct($structName));
@@ -40,10 +52,18 @@ class StructContainer extends ModelContainer
     }
     /**
      * @see \WsdlToPhp\PackageGenerator\ModelContainer\AbstractModelContainer::get()
-     * @return Struct
+     * @return Struct|null
      */
     public function get($value, $key = parent::KEY_NAME)
     {
         return parent::get($value, $key);
+    }
+    /**
+     * @see \WsdlToPhp\PackageGenerator\ModelContainer\AbstractModelContainer::getAs()
+     * @return Struct|null
+     */
+    public function getAs(array $properties)
+    {
+        return parent::getAs($properties);
     }
 }
