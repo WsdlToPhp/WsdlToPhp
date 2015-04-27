cf974b1fb178f5534a8d8da90c430e984735a098
Mikael DELSOL <delsol.mikael@gmail.com>
2015-04-27 10:01:53 +0200

    enhance comments and methods, add getMethodByName and hasMethod methods

--- src/WsdlToPhp/PackageGenerator/ModelContainer/MethodContainer.php
+++ src/WsdlToPhp/PackageGenerator/ModelContainer/MethodContainer.php
@@ -4,8 +4,49 @@ namespace WsdlToPhp\PackageGenerator\ModelContainer;
 
 class MethodContainer extends ModelContainer
 {
+    const KEY_PARAMETER_TYPE = 'parameterType';
+    /**
+     * @see \WsdlToPhp\PackageGenerator\ModelContainer\ModelContainer::modelClass()
+     * @return string
+     */
     protected function modelClass()
     {
         return 'WsdlToPhp\\PackageGenerator\\Model\\Method';
+    }
+    /**
+     * @param sring $name
+     * @return Method|null
+     */
+    public function getMethodByName($name)
+    {
+        return $this->get($name, parent::KEY_NAME);
+    }
+    /**
+     * @param string $name
+     * @param mixed $parameterType
+     * @return boolean
+     */
+    public function hasMethod($name, $parameterType)
+    {
+        return null !== $this->getAs(array(
+            parent::KEY_NAME         => $name,
+            self::KEY_PARAMETER_TYPE => $parameterType,
+        ));
+    }
+    /**
+     * @see \WsdlToPhp\PackageGenerator\ModelContainer\AbstractModelContainer::get()
+     * @return Method|null
+     */
+    public function get($value, $key = parent::KEY_NAME)
+    {
+        return parent::get($value, $key);
+    }
+    /**
+     * @see \WsdlToPhp\PackageGenerator\ModelContainer\AbstractModelContainer::getAs()
+     * @return Method|null
+     */
+    public function getAs(array $properties)
+    {
+        return parent::getAs($properties);
     }
 }
