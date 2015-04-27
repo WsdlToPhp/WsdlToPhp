d96999063364cf8bb270c3be4955ec162eab37c6
Mikael DELSOL <delsol.mikael@gmail.com>
2015-04-27 10:03:30 +0200

    add unit tests for container classes

---
+++ src/WsdlToPhp/PackageGenerator/Tests/ModelContainer/StructContainerTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace WsdlToPhp\PackageGenerator\Tests\ModelContainer;
+
+use WsdlToPhp\PackageGenerator\Tests\Model\StructTest;
+
+use WsdlToPhp\PackageGenerator\ModelContainer\StructContainer;
+
+use WsdlToPhp\PackageGenerator\Tests\TestCase;
+
+class StructContainerTest extends TestCase
+{
+    /**
+     * @return StructContainer
+     */
+    public static function instance()
+    {
+        $structContainer = new StructContainer();
+        $structContainer->add(StructTest::instance('Foo', true));
+        $structContainer->add(StructTest::instance('Bar', false));
+        return $structContainer;
+    }
+    /**
+     *
+     */
+    public function testGetStructByName()
+    {
+        $structContainer = self::instance();
+
+        $this->assertInstanceOf('\\WsdlToPhp\\PackageGenerator\\Model\\Struct', $structContainer->getStructByName('Foo'));
+        $this->assertInstanceOf('\\WsdlToPhp\\PackageGenerator\\Model\\Struct', $structContainer->getStructByName('Bar'));
+        $this->assertNull($structContainer->getStructByName('bar'));
+    }
+}
