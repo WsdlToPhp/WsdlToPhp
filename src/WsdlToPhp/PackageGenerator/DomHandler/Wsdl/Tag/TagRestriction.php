11264358ad72b206bb585e8e8f27264835f7a9ea
Mikael DELSOL <delsol.mikael@gmail.com>
2015-04-28 01:39:59 +0200

    add isEnumeration and getRestrictions methods

--- src/WsdlToPhp/PackageGenerator/DomHandler/Wsdl/Tag/TagRestriction.php
+++ src/WsdlToPhp/PackageGenerator/DomHandler/Wsdl/Tag/TagRestriction.php
@@ -2,6 +2,22 @@
 
 namespace WsdlToPhp\PackageGenerator\DomHandler\Wsdl\Tag;
 
+use WsdlToPhp\PackageGenerator\DomHandler\Wsdl\Wsdl as WsdlDocument;
+
 class TagRestriction extends AbstractTag
 {
+    /**
+     * @return boolean
+     */
+    public function isEnumeration()
+    {
+        return count($this->getEnumerations()) === 0;
+    }
+    /**
+     * @return array[TagEnumeration]
+     */
+    public function getEnumerations()
+    {
+        return $this->getChildrenByName(WsdlDocument::TAG_ENUMERATION);
+    }
 }
