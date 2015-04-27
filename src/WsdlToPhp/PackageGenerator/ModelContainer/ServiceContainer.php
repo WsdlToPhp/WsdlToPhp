c7467d842ea71fcc655db4af62d7390d6ca08968
Mikael DELSOL <delsol.mikael@gmail.com>
2015-04-27 14:10:09 +0200

    update method

--- src/WsdlToPhp/PackageGenerator/ModelContainer/ServiceContainer.php
+++ src/WsdlToPhp/PackageGenerator/ModelContainer/ServiceContainer.php
@@ -7,10 +7,10 @@ use WsdlToPhp\PackageGenerator\Model\Service;
 class ServiceContainer extends ModelContainer
 {
     /**
-     * @see \WsdlToPhp\PackageGenerator\ModelContainer\ModelContainer::modelClass()
+     * @see \WsdlToPhp\PackageGenerator\ModelContainer\ModelContainer::objectClass()
      * @return string
      */
-    protected function modelClass()
+    protected function objectClass()
     {
         return 'WsdlToPhp\\PackageGenerator\\Model\\Service';
     }
