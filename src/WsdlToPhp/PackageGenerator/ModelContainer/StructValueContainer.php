8a6bf276fa3b50214724eff46825cd4dc5280a89
Mikael DELSOL <delsol.mikael@gmail.com>
2015-04-27 10:00:41 +0200

    enchance comments and methods

--- src/WsdlToPhp/PackageGenerator/ModelContainer/StructValueContainer.php
+++ src/WsdlToPhp/PackageGenerator/ModelContainer/StructValueContainer.php
@@ -4,8 +4,28 @@ namespace WsdlToPhp\PackageGenerator\ModelContainer;
 
 class StructValueContainer extends ModelContainer
 {
+    /**
+     * @see \WsdlToPhp\PackageGenerator\ModelContainer\ModelContainer::modelClass()
+     * @return string
+     */
     protected function modelClass()
     {
         return 'WsdlToPhp\\PackageGenerator\\Model\\StructValue';
+    }
+    /**
+     * @see \WsdlToPhp\PackageGenerator\ModelContainer\AbstractModelContainer::get()
+     * @return StructValue|null
+     */
+    public function get($value, $key = parent::KEY_NAME)
+    {
+        return parent::get($value, $key);
+    }
+    /**
+     * @see \WsdlToPhp\PackageGenerator\ModelContainer\AbstractModelContainer::getAs()
+     * @return StructValue|null
+     */
+    public function getAs(array $properties)
+    {
+        return parent::getAs($properties);
     }
 }
