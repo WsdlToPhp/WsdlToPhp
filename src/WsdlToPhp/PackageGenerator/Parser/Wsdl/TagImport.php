927519c0543c0bc6090c89f836b421b1b5f2bd57
Mikael DELSOL <delsol.mikael@gmail.com>
2015-04-27 19:13:38 +0200

    set parser behaviour

--- src/WsdlToPhp/PackageGenerator/Parser/Wsdl/TagImport.php
+++ src/WsdlToPhp/PackageGenerator/Parser/Wsdl/TagImport.php
@@ -2,20 +2,38 @@
 
 namespace WsdlToPhp\PackageGenerator\Parser\Wsdl;
 
+use WsdlToPhp\PackageGenerator\Generator\Utils;
 use WsdlToPhp\PackageGenerator\Model\Wsdl;
+use WsdlToPhp\PackageGenerator\DomHandler\Wsdl\Wsdl as WsdlDocument;
 
 class TagImport extends AbstractParser
 {
+    /**
+     * @see \WsdlToPhp\PackageGenerator\Parser\Wsdl\AbstractParser::getTags()
+     * @return array[\WsdlToPhp\PackageGenerator\DomHandler\Wsdl\Tag\TagImport]
+     */
+    public function getTags()
+    {
+        return parent::getTags();
+    }
 	/**
      * @see \WsdlToPhp\PackageGenerator\Parser\Wsdl\AbstractParser::parseWsdl()
      */
     protected function parseWsdl(Wsdl $wsdl)
     {
+        foreach ($this->getTags() as $tag) {
+            if ($tag->getLocationAttribute() != '') {
+                $finalLocation = Utils::resolveCompletePath($wsdl->getName(), $tag->getLocationAttribute());
+                $this->generator->addWsdl($finalLocation);
+            }
+        }
     }
 	/**
      * @see \WsdlToPhp\PackageGenerator\Parser\Wsdl\AbstractParser::parsingTag()
+     * @return string
      */
     protected function parsingTag()
     {
+        return WsdlDocument::TAG_IMPORT;
     }
 }
