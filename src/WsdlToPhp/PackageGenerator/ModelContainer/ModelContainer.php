211a878609b3c37d3d460d3a51bce922a2a13ca4
Mikael DELSOL <delsol.mikael@gmail.com>
2015-04-27 10:01:09 +0200

    enchance comments and methods

--- src/WsdlToPhp/PackageGenerator/ModelContainer/ModelContainer.php
+++ src/WsdlToPhp/PackageGenerator/ModelContainer/ModelContainer.php
@@ -4,8 +4,28 @@ namespace WsdlToPhp\PackageGenerator\ModelContainer;
 
 class ModelContainer extends AbstractModelContainer
 {
+    /**
+     * @see \WsdlToPhp\PackageGenerator\ModelContainer\AbstractModelContainer::modelClass()
+     * @return string
+     */
     protected function modelClass()
     {
         return 'WsdlToPhp\\PackageGenerator\\Model\\EmptyModel';
+    }
+    /**
+     * @see \WsdlToPhp\PackageGenerator\ModelContainer\AbstractModelContainer::get()
+     * @return EmptyModel|null
+     */
+    public function get($value, $key = parent::KEY_NAME)
+    {
+        return parent::get($value, $key);
+    }
+    /**
+     * @see \WsdlToPhp\PackageGenerator\ModelContainer\AbstractModelContainer::getAs()
+     * @return EmptyModel|null
+     */
+    public function getAs(array $properties)
+    {
+        return parent::getAs($properties);
     }
 }
