b06a04a72c886d09d5bc7250e7f027db617726d7
Mikael DELSOL <delsol.mikael@gmail.com>
2015-04-27 10:53:22 +0200

    ehance comments and methods, add getStructAttributeByName method

--- src/WsdlToPhp/PackageGenerator/ModelContainer/StructAttributeContainer.php
+++ src/WsdlToPhp/PackageGenerator/ModelContainer/StructAttributeContainer.php
@@ -2,10 +2,40 @@
 
 namespace WsdlToPhp\PackageGenerator\ModelContainer;
 
+use WsdlToPhp\PackageGenerator\Model\StructAttribute;
+
 class StructAttributeContainer extends ModelContainer
 {
+    /**
+     * @see \WsdlToPhp\PackageGenerator\ModelContainer\ModelContainer::modelClass()
+     * @return string
+     */
     protected function modelClass()
     {
         return 'WsdlToPhp\\PackageGenerator\\Model\\StructAttribute';
+    }
+    /**
+     * @param string $name
+     * @return StructAttribute|null
+     */
+    public function getStructAttributeByName($name)
+    {
+        return $this->get($name, parent::KEY_NAME);
+    }
+    /**
+     * @see \WsdlToPhp\PackageGenerator\ModelContainer\AbstractModelContainer::get()
+     * @return StructAttribute|null
+     */
+    public function get($value, $key = parent::KEY_NAME)
+    {
+        return parent::get($value, $key);
+    }
+    /**
+     * @see \WsdlToPhp\PackageGenerator\ModelContainer\AbstractModelContainer::getAs()
+     * @return StructAttribute|null
+     */
+    public function getAs(array $properties)
+    {
+        return parent::getAs($properties);
     }
 }
