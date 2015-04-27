c88be899a4066bb719dbc0ca271bb232480a9f91
Mikael DELSOL <delsol.mikael@gmail.com>
2015-04-27 22:08:07 +0200

    add utility method generatorInstance to get Generator instance with imported schemas

--- src/WsdlToPhp/PackageGenerator/Tests/Parser/Wsdl/WsdlParser.php
+++ src/WsdlToPhp/PackageGenerator/Tests/Parser/Wsdl/WsdlParser.php
@@ -2,6 +2,9 @@
 
 namespace WsdlToPhp\PackageGenerator\Tests\Parser\Wsdl;
 
+use WsdlToPhp\PackageGenerator\Generator\Generator;
+use WsdlToPhp\PackageGenerator\Parser\Wsdl\TagImport;
+use WsdlToPhp\PackageGenerator\Parser\Wsdl\TagInclude;
 use WsdlToPhp\PackageGenerator\Tests\TestCase;
 
 class WsdlParser extends TestCase
@@ -33,5 +36,28 @@ class WsdlParser extends TestCase
     public static function schemaImageViewServicePath()
     {
         return dirname(__FILE__) . '/../../resources/imageViewCommon.xsd';
+    }
+    /**
+     * @return string
+     */
+    public static function wsdlBingPath()
+    {
+        return dirname(__FILE__) . '/../../resources/bingsearch.wsdl';
+    }
+    /**
+     * @param srting $wsdlPath
+     * @return Generator
+     */
+    public static function generatorInstance($wsdlPath)
+    {
+        $generator = new Generator($wsdlPath);
+        $parsers = array(
+            new TagInclude($generator),
+            new TagImport($generator),
+        );
+        foreach ($parsers as $parser) {
+            $parser->parse();
+        }
+        return $generator;
     }
 }
