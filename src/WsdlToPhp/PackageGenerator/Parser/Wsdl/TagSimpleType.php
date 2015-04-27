c229275ab4d5783ebba626a66803d30f15e7c80d
Mikael DELSOL <delsol.mikael@gmail.com>
2015-04-28 01:41:16 +0200

    implement parser behaviour

--- src/WsdlToPhp/PackageGenerator/Parser/Wsdl/TagSimpleType.php
+++ src/WsdlToPhp/PackageGenerator/Parser/Wsdl/TagSimpleType.php
@@ -3,19 +3,33 @@
 namespace WsdlToPhp\PackageGenerator\Parser\Wsdl;
 
 use WsdlToPhp\PackageGenerator\Model\Wsdl;
+use WsdlToPhp\PackageGenerator\DomHandler\Wsdl\Wsdl as WsdlDocument;
 
 class TagSimpleType extends AbstractParser
 {
+    /**
+     * @see \WsdlToPhp\PackageGenerator\Parser\Wsdl\AbstractParser::getTags()
+     * @return array[\WsdlToPhp\PackageGenerator\DomHandler\Wsdl\Tag\TagSimpleType]
+     */
+    public function getTags()
+    {
+        return parent::getTags();
+    }
 	/**
      * @see \WsdlToPhp\PackageGenerator\Parser\Wsdl\AbstractParser::parseWsdl()
      */
     protected function parseWsdl(Wsdl $wsdl)
     {
+        foreach ($this->getTags() as $tag) {
+            $this->parseRestrictions($tag);
+            $this->parseAnnotations($tag);
+        }
     }
 	/**
      * @see \WsdlToPhp\PackageGenerator\Parser\Wsdl\AbstractParser::parsingTag()
      */
     protected function parsingTag()
     {
+        return WsdlDocument::TAG_SIMPLE_CONTENT;
     }
 }
