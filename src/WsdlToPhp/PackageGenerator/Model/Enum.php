8657e783295cac79a0c96a7545abbf22be9c358c
Mikael DELSOL <delsol.mikael@gmail.com>
2015-04-27 10:00:01 +0200

    use new constructor way of Struct

--- src/WsdlToPhp/PackageGenerator/Model/Enum.php
+++ src/WsdlToPhp/PackageGenerator/Model/Enum.php
@@ -6,7 +6,6 @@ class Enum extends Struct
 {
     public function __construct($name, $isStruct = true)
     {
-        parent::__construct($name, $isStruct);
-        $this->setIsRestriction(true);
+        parent::__construct($name, $isStruct, true);
     }
 }
