25c8001d68b5efe1d42c908a669f62e1288ed8dc
Mikael DELSOL <delsol.mikael@gmail.com>
2015-04-28 23:40:10 +0200

    add behaviour for unit test with bing

--- src/WsdlToPhp/PackageGenerator/Tests/Parser/Wsdl/TagSimpleTypeTest.php
+++ src/WsdlToPhp/PackageGenerator/Tests/Parser/Wsdl/TagSimpleTypeTest.php
@@ -4,6 +4,9 @@ namespace WsdlToPhp\PackageGenerator\Tests\Parser\Wsdl;
 
 use WsdlToPhp\PackageGenerator\Generator\AbstractContainer;
 use WsdlToPhp\PackageGenerator\Parser\Wsdl\TagSimpleType;
+use WsdlToPhp\PackageGenerator\Model\Struct;
+use WsdlToPhp\PackageGenerator\ModelContainer\StructValueContainer;
+use WsdlToPhp\PackageGenerator\Model\StructValue;
 
 class TagSimpleTypeTest extends WsdlParser
 {
@@ -31,11 +34,34 @@ class TagSimpleTypeTest extends WsdlParser
     /**
      *
      */
-    public function testGetBingSimpleTypes()
+    public function testParseBing()
     {
         $tagSimpleTypeParser = self::bingInstance();
         AbstractContainer::purgeAllCache();
 
         $tagSimpleTypeParser->parse();
+
+        $ok = false;
+        foreach ($tagSimpleTypeParser->getGenerator()->getStructs() as $struct) {
+            if ($struct instanceof Struct && $struct->getIsRestriction() === true) {
+                if ($struct->getName() === 'AdultOption') {
+                    $values = new StructValueContainer();
+                    $values->add(new StructValue('Off', 0, $struct));
+                    $values->add(new StructValue('Moderate', 1, $struct));
+                    $values->add(new StructValue('Strict', 2, $struct));
+
+                    $this->assertEquals($values, $struct->getValues());
+                    $ok = true;
+                } elseif ($struct->getName() === 'SearchOption') {
+                    $values = new StructValueContainer();
+                    $values->add(new StructValue('DisableLocationDetection', 0, $struct));
+                    $values->add(new StructValue('EnableHighlighting', 1, $struct));
+
+                    $this->assertEquals($values, $struct->getValues());
+                    $ok = true;
+                }
+            }
+        }
+        $this->assertTrue((bool)$ok);
     }
 }
