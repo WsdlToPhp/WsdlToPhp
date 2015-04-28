04e16f4ccb54d980c6f20727462d6470b79b6428
Mikael DELSOL <delsol.mikael@gmail.com>
2015-04-29 00:49:02 +0200

    add possible parent tags for documentation that we want to catch

--- src/WsdlToPhp/PackageGenerator/DomHandler/Wsdl/Tag/TagDocumentation.php
+++ src/WsdlToPhp/PackageGenerator/DomHandler/Wsdl/Tag/TagDocumentation.php
@@ -2,6 +2,8 @@
 
 namespace WsdlToPhp\PackageGenerator\DomHandler\Wsdl\Tag;
 
+use WsdlToPhp\PackageGenerator\DomHandler\Wsdl\Wsdl as WsdlDocument;
+
 class TagDocumentation extends AbstractTag
 {
     /**
@@ -10,5 +12,15 @@ class TagDocumentation extends AbstractTag
     public function getContent()
     {
         return $this->getNodeValue();
+    }
+    /**
+     * @see \WsdlToPhp\PackageGenerator\DomHandler\Wsdl\Tag\AbstractTag::getSuitableParentTags()
+     */
+    public function getSuitableParentTags(array $additionalTags = array())
+    {
+        return array_merge(parent::getSuitableParentTags($additionalTags), array(
+            WsdlDocument::TAG_OPERATION,
+            WsdlDocument::TAG_ENUMERATION,
+        ));
     }
 }
