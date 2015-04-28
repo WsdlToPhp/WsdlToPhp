e5e7bf4cb05f5c0673557cf4ec1d229c2f23a2f3
Mikael DELSOL <delsol.mikael@gmail.com>
2015-04-29 00:49:52 +0200

    define behaviour of complex type parser

--- src/WsdlToPhp/PackageGenerator/Parser/Wsdl/TagComplexType.php
+++ src/WsdlToPhp/PackageGenerator/Parser/Wsdl/TagComplexType.php
@@ -3,19 +3,43 @@
 namespace WsdlToPhp\PackageGenerator\Parser\Wsdl;
 
 use WsdlToPhp\PackageGenerator\Model\Wsdl;
+use WsdlToPhp\PackageGenerator\DomHandler\Wsdl\Wsdl as WsdlDocument;
+use WsdlToPhp\PackageGenerator\DomHandler\Wsdl\Tag\AbstractTag as Tag;
+use WsdlToPhp\PackageGenerator\Model\Struct;
 
 class TagComplexType extends AbstractParser
 {
+    /**
+     * @see \WsdlToPhp\PackageGenerator\Parser\Wsdl\AbstractParser::getTags()
+     * @return array[\WsdlToPhp\PackageGenerator\DomHandler\Wsdl\Tag\TagComplexType]
+     */
+    public function getTags()
+    {
+        return parent::getTags();
+    }
 	/**
      * @see \WsdlToPhp\PackageGenerator\Parser\Wsdl\AbstractParser::parseWsdl()
      */
     protected function parseWsdl(Wsdl $wsdl)
     {
+        foreach ($this->getTags() as $tag) {
+            $this->parseRestrictions($tag);
+            $this->parseAnnotations($tag);
+        }
     }
 	/**
      * @see \WsdlToPhp\PackageGenerator\Parser\Wsdl\AbstractParser::parsingTag()
      */
     protected function parsingTag()
     {
+        return WsdlDocument::TAG_COMPLEX_TYPE;
+    }
+    /**
+     * @see \WsdlToPhp\PackageGenerator\Parser\Wsdl\AbstractParser::getModel()
+     * @return Struct
+     */
+    protected function getModel(Tag $tag)
+    {
+        return $this->generator->getStruct($tag->getAttributeName());
     }
 }
