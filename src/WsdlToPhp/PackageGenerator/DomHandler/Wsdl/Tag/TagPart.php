601e7c4cebe0671df5adc875f93f31f243e024d1
Mikael DELSOL <delsol.mikael@gmail.com>
2015-04-28 10:59:35 +0200

    add getAttributeElement and getAttributeType methods

--- src/WsdlToPhp/PackageGenerator/DomHandler/Wsdl/Tag/TagPart.php
+++ src/WsdlToPhp/PackageGenerator/DomHandler/Wsdl/Tag/TagPart.php
@@ -4,4 +4,21 @@ namespace WsdlToPhp\PackageGenerator\DomHandler\Wsdl\Tag;
 
 class TagPart extends AbstractTag
 {
+    const
+        ATTRIBUTE_ELEMENT  = 'element',
+        ATTRIBUTE_TYPE  = 'type';
+    /**
+     * @return string
+     */
+    public function getAttributeElement()
+    {
+        return $this->hasAttribute(self::ATTRIBUTE_ELEMENT) === true ? $this->getAttribute(self::ATTRIBUTE_ELEMENT)->getValue() : '';
+    }
+    /**
+     * @return string
+     */
+    public function getAttributeType()
+    {
+        return $this->hasAttribute(self::ATTRIBUTE_TYPE) === true ? $this->getAttribute(self::ATTRIBUTE_TYPE)->getValue() : '';
+    }
 }
