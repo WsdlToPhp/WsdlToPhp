246c7e0489fac09b89994dd789574f23d3250efb
Mikael DELSOL <delsol.mikael@gmail.com>
2015-04-30 23:30:54 +0200

    implement attribute parser behaviour

--- src/WsdlToPhp/PackageGenerator/Parser/Wsdl/TagAttribute.php
+++ src/WsdlToPhp/PackageGenerator/Parser/Wsdl/TagAttribute.php
@@ -2,20 +2,33 @@
 
 namespace WsdlToPhp\PackageGenerator\Parser\Wsdl;
 
+use WsdlToPhp\PackageGenerator\DomHandler\Wsdl\Wsdl as WsdlDocument;
 use WsdlToPhp\PackageGenerator\Model\Wsdl;
 
-class TagAttribute extends AbstractParser
+class TagAttribute extends AbstractTagParser
 {
+    /**
+     * @see \WsdlToPhp\PackageGenerator\Parser\Wsdl\AbstractParser::getTags()
+     * @return array[\WsdlToPhp\PackageGenerator\DomHandler\Wsdl\Tag\TagAttribute]
+     */
+    public function getTags()
+    {
+        return parent::getTags();
+    }
 	/**
      * @see \WsdlToPhp\PackageGenerator\Parser\Wsdl\AbstractParser::parseWsdl()
      */
     protected function parseWsdl(Wsdl $wsdl)
     {
+        foreach ($this->getTags() as $tag) {
+            $this->parseAttribute($tag);
+        }
     }
 	/**
      * @see \WsdlToPhp\PackageGenerator\Parser\Wsdl\AbstractParser::parsingTag()
      */
     protected function parsingTag()
     {
+        return WsdlDocument::TAG_ATTRIBUTE;
     }
 }
