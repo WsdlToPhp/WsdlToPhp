8c1a2975bb65e607f48bd12433b2387e9b76dcb3
Mikael DELSOL <delsol.mikael@gmail.com>
2015-04-30 14:51:10 +0200

    implement enumeration tag parser behaviour

--- src/WsdlToPhp/PackageGenerator/Parser/Wsdl/TagEnumeration.php
+++ src/WsdlToPhp/PackageGenerator/Parser/Wsdl/TagEnumeration.php
@@ -2,20 +2,33 @@
 
 namespace WsdlToPhp\PackageGenerator\Parser\Wsdl;
 
+use WsdlToPhp\PackageGenerator\DomHandler\Wsdl\Wsdl as WsdlDocument;
 use WsdlToPhp\PackageGenerator\Model\Wsdl;
 
-class TagEnumeration extends AbstractParser
+class TagEnumeration extends AbstractTagParser
 {
+    /**
+     * @see \WsdlToPhp\PackageGenerator\Parser\Wsdl\AbstractParser::getTags()
+     * @return array[\WsdlToPhp\PackageGenerator\DomHandler\Wsdl\Tag\TagEnumeration]
+     */
+    public function getTags()
+    {
+        return parent::getTags();
+    }
 	/**
      * @see \WsdlToPhp\PackageGenerator\Parser\Wsdl\AbstractParser::parseWsdl()
      */
     protected function parseWsdl(Wsdl $wsdl)
     {
+        foreach ($this->getTags() as $tag) {
+            $this->parseEnumeration($tag);
+        }
     }
 	/**
      * @see \WsdlToPhp\PackageGenerator\Parser\Wsdl\AbstractParser::parsingTag()
      */
     protected function parsingTag()
     {
+        return WsdlDocument::TAG_ENUMERATION;
     }
 }
