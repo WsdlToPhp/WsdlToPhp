369771155a40d05abe36d22c5cf3d768386e898b
Mikael DELSOL <delsol.mikael@gmail.com>
2015-04-30 14:24:27 +0200

    define parser behaviour

--- src/WsdlToPhp/PackageGenerator/Parser/Wsdl/TagDocumentation.php
+++ src/WsdlToPhp/PackageGenerator/Parser/Wsdl/TagDocumentation.php
@@ -2,20 +2,33 @@
 
 namespace WsdlToPhp\PackageGenerator\Parser\Wsdl;
 
+use WsdlToPhp\PackageGenerator\DomHandler\Wsdl\Wsdl as WsdlDocument;
 use WsdlToPhp\PackageGenerator\Model\Wsdl;
 
 class TagDocumentation extends AbstractParser
 {
+    /**
+     * @see \WsdlToPhp\PackageGenerator\Parser\Wsdl\AbstractParser::getTags()
+     * @return array[\WsdlToPhp\PackageGenerator\DomHandler\Wsdl\Tag\TagDocumentation]
+     */
+    public function getTags()
+    {
+        return parent::getTags();
+    }
 	/**
      * @see \WsdlToPhp\PackageGenerator\Parser\Wsdl\AbstractParser::parseWsdl()
      */
     protected function parseWsdl(Wsdl $wsdl)
     {
+        foreach ($this->getTags() as $tag) {
+            $this->parseDocumentation($tag);
+        }
     }
 	/**
      * @see \WsdlToPhp\PackageGenerator\Parser\Wsdl\AbstractParser::parsingTag()
      */
     protected function parsingTag()
     {
+        return WsdlDocument::TAG_DOCUMENTATION;
     }
 }
