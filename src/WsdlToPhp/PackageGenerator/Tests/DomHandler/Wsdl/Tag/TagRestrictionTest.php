6c41a8b783341f80fb3407ab11d4c99b6831ae51
Mikael DELSOL <delsol.mikael@gmail.com>
2015-04-30 14:24:45 +0200

    add new instances method

--- src/WsdlToPhp/PackageGenerator/Tests/DomHandler/Wsdl/Tag/TagRestrictionTest.php
+++ src/WsdlToPhp/PackageGenerator/Tests/DomHandler/Wsdl/Tag/TagRestrictionTest.php
@@ -4,7 +4,6 @@ namespace WsdlToPhp\PackageGenerator\Test\DomHandler\Wsdl\Tag;
 
 use WsdlToPhp\PackageGenerator\Tests\TestCase;
 use WsdlToPhp\PackageGenerator\Tests\Model\WsdlTest;
-use WsdlToPhp\PackageGenerator\DomHandler\Wsdl\Wsdl;
 
 class TagRestrictionTest extends TestCase
 {
--- src/WsdlToPhp/PackageGenerator/Tests/Model/WsdlTest.php
+++ src/WsdlToPhp/PackageGenerator/Tests/Model/WsdlTest.php
@@ -22,6 +22,20 @@ class WsdlTest extends TestCase
         return new Wsdl(dirname(__FILE__) . '/../resources/PartnerService.wsdl', file_get_contents(dirname(__FILE__) . '/../resources/PartnerService.wsdl'));
     }
     /**
+     * @return Wsdl
+     */
+    public static function imageServiceViewInstance()
+    {
+        return new Wsdl(dirname(__FILE__) . '/../resources/ImageViewService.local.wsdl', file_get_contents(dirname(__FILE__) . '/../resources/ImageViewService.local.wsdl'));
+    }
+    /**
+     * @return Wsdl
+     */
+    public static function imageServiceViewAvailRequestInstance()
+    {
+        return new Wsdl(dirname(__FILE__) . '/../resources/availableImagesRequest.xsd', file_get_contents(dirname(__FILE__) . '/../resources/availableImagesRequest.xsd'));
+    }
+    /**
      *
      */
     public function testGetName()
