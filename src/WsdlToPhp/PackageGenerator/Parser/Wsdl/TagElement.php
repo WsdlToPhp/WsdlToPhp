6ef44631fd67225e4b4adb934635f2aa1d8bdf39
Mikael DELSOL <delsol.mikael@gmail.com>
2015-05-01 14:06:05 +0200

    add element parser behaviour

---
+++ src/WsdlToPhp/PackageGenerator/Parser/Wsdl/TagElement.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace WsdlToPhp\PackageGenerator\Parser\Wsdl;
+
+use WsdlToPhp\PackageGenerator\DomHandler\Wsdl\Wsdl as WsdlDocument;
+use WsdlToPhp\PackageGenerator\Model\Wsdl;
+
+class TagElement extends AbstractTagParser
+{
+    /**
+     * @see \WsdlToPhp\PackageGenerator\Parser\Wsdl\AbstractParser::getTags()
+     * @return array[\WsdlToPhp\PackageGenerator\DomHandler\Wsdl\Tag\TagElement]
+     */
+    public function getTags()
+    {
+        return parent::getTags();
+    }
+    /**
+     * @see \WsdlToPhp\PackageGenerator\Parser\Wsdl\AbstractParser::parseWsdl()
+     */
+    protected function parseWsdl(Wsdl $wsdl)
+    {
+        foreach ($this->getTags() as $tag) {
+            $this->parseElement($tag);
+        }
+    }
+    /**
+     * @see \WsdlToPhp\PackageGenerator\Parser\Wsdl\AbstractParser::parsingTag()
+     */
+    protected function parsingTag()
+    {
+        return WsdlDocument::TAG_ELEMENT;
+    }
+}
