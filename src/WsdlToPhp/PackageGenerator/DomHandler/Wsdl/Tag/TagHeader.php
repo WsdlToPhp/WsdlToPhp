164f753b87718b8ef27a19cf6d2cf3c8a01f1978
Mikael DELSOL <delsol.mikael@gmail.com>
2015-05-01 15:34:34 +0200

    add constants for attributes and methods for each important attribute

--- src/WsdlToPhp/PackageGenerator/DomHandler/Wsdl/Tag/TagHeader.php
+++ src/WsdlToPhp/PackageGenerator/DomHandler/Wsdl/Tag/TagHeader.php
@@ -2,6 +2,55 @@
 
 namespace WsdlToPhp\PackageGenerator\DomHandler\Wsdl\Tag;
 
+use WsdlToPhp\PackageGenerator\DomHandler\Wsdl\Wsdl as WsdlDocument;
+use WsdlToPhp\PackageGenerator\DomHandler\AbstractAttributeHandler as Attribute;
+
 class TagHeader extends AbstractTag
 {
+    const
+        ATTRIBUTE_MESSAGE  = 'message',
+        ATTRIBUTE_PART     = 'part',
+        ATTRIBUTE_REQUIRED = 'required';
+    /**
+     * @return TagOperation|null
+     */
+    public function getParentOperation()
+    {
+        return $this->getStrictParent(WsdlDocument::TAG_OPERATION);
+    }
+    /**
+     * @return TagInput|null
+     */
+    public function getParentInput()
+    {
+        return $this->getStrictParent(WsdlDocument::TAG_INPUT);
+    }
+    /**
+     * @return string
+     */
+    public function getAttributePart()
+    {
+        return $this->getAttribute(self::ATTRIBUTE_PART) !== null ? $this->getAttribute(self::ATTRIBUTE_PART)->getValue() : '';
+    }
+    /**
+     * @return string
+     */
+    public function getAttributeMessage()
+    {
+        return $this->getAttribute(self::ATTRIBUTE_MESSAGE) !== null ? $this->getAttribute(self::ATTRIBUTE_MESSAGE)->getValue() : '';
+    }
+    /**
+     * @return string
+     */
+    public function getAttributeRequired()
+    {
+        return $this->getAttribute(self::ATTRIBUTE_REQUIRED) !== null ? $this->getAttribute(self::ATTRIBUTE_REQUIRED)->getValue() : '';
+    }
+    /**
+     * @return string
+     */
+    public function getAttributeNamespace()
+    {
+        return $this->getAttribute(Attribute::ATTRIBUTE_NAMESPACE) !== null ? $this->getAttribute(Attribute::ATTRIBUTE_NAMESPACE)->getValue() : '';
+    }
 }
