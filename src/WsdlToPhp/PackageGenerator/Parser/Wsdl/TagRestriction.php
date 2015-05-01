cea6e2314f2750ea19a81a6355115c96be0e3463
Mikael DELSOL <delsol.mikael@gmail.com>
2015-05-01 13:56:44 +0200

    define restriction tag parser baheviour

---
+++ src/WsdlToPhp/PackageGenerator/Parser/Wsdl/TagRestriction.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace WsdlToPhp\PackageGenerator\Parser\Wsdl;
+
+use WsdlToPhp\PackageGenerator\DomHandler\Wsdl\Wsdl as WsdlDocument;
+use WsdlToPhp\PackageGenerator\Model\Wsdl;
+
+class TagRestriction extends AbstractTagParser
+{
+    /**
+     * @see \WsdlToPhp\PackageGenerator\Parser\Wsdl\AbstractParser::getTags()
+     * @return array[\WsdlToPhp\PackageGenerator\DomHandler\Wsdl\Tag\TagRestriction]
+     */
+    public function getTags()
+    {
+        return parent::getTags();
+    }
+    /**
+     * @see \WsdlToPhp\PackageGenerator\Parser\Wsdl\AbstractParser::parseWsdl()
+     */
+    protected function parseWsdl(Wsdl $wsdl)
+    {
+        foreach ($this->getTags() as $tag) {
+            if ($tag->isEnumeration() === false) {
+                $this->parseRestriction($tag);
+            }
+        }
+    }
+    /**
+     * @see \WsdlToPhp\PackageGenerator\Parser\Wsdl\AbstractParser::parsingTag()
+     */
+    protected function parsingTag()
+    {
+        return WsdlDocument::TAG_RESTRICTION;
+    }
+}
