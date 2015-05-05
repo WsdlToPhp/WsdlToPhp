f00e32de83c34e279d68f8b231ce8c0bb4c725ee
Mikael DELSOL <delsol.mikael@gmail.com>
2015-05-06 01:26:38 +0200

    add unit tests for additional methods

--- src/WsdlToPhp/PackageGenerator/Tests/Parser/Wsdl/TagImportTest.php
+++ src/WsdlToPhp/PackageGenerator/Tests/Parser/Wsdl/TagImportTest.php
@@ -8,6 +8,7 @@ use WsdlToPhp\PackageGenerator\Parser\Wsdl\TagImport;
 use WsdlToPhp\PackageGenerator\Generator\Generator;
 use WsdlToPhp\PackageGenerator\Model\Wsdl;
 use WsdlToPhp\PackageGenerator\Model\Schema;
+use WsdlToPhp\PackageGenerator\DomHandler\Wsdl\Wsdl as WsdlDocument;
 
 class TagImportTest extends WsdlParser
 {
@@ -62,5 +63,61 @@ class TagImportTest extends WsdlParser
 
         $tagImportParser->getGenerator()->getWsdl(0)->getContent()->getExternalSchemas()->rewind();
         $this->assertEquals($schemaContainer, $tagImportParser->getGenerator()->getWsdl(0)->getContent()->getExternalSchemas());
+    }
+    /**
+     *
+     */
+    public function testGetRestrictionFromExternalSchemas()
+    {
+        $tagImportParser = self::instance();
+        AbstractObjectContainer::purgeAllCache();
+
+        $tagImportParser->parse();
+
+        $this->assertInstanceOf('\\WsdlToPhp\\PackageGenerator\\DomHandler\\Wsdl\\Tag\\TagRestriction', $tagImportParser->getGenerator()->getWsdl(0)->getContent()->getElementByName(WsdlDocument::TAG_RESTRICTION, true));
+    }
+    /**
+     *
+     */
+    public function testGetEnumerationByAttributesFromExternalSchemas()
+    {
+        $tagImportParser = self::instance();
+        AbstractObjectContainer::purgeAllCache();
+
+        $tagImportParser->parse();
+
+        $this->assertInstanceOf('\\WsdlToPhp\\PackageGenerator\\DomHandler\\Wsdl\\Tag\\TagEnumeration', $tagImportParser->getGenerator()->getWsdl(0)->getContent()->getElementByNameAndAttributes(WsdlDocument::TAG_ENUMERATION, array(
+            'value' => 'InternalServerError',
+        ), true));
+    }
+    /**
+     *
+     */
+    public function testGetElementsFromExternalSchemas()
+    {
+        $tagImportParser = self::instance();
+        AbstractObjectContainer::purgeAllCache();
+
+        $tagImportParser->parse();
+        $restrictions = $tagImportParser->getGenerator()->getWsdl(0)->getContent()->getElementsByName(WsdlDocument::TAG_RESTRICTION, true);
+
+        $this->assertNotEmpty($restrictions);
+        $this->assertContainsOnlyInstancesOf('\\WsdlToPhp\\PackageGenerator\\DomHandler\\Wsdl\\Tag\\TagRestriction', $restrictions);
+    }
+    /**
+     *
+     */
+    public function testGetElementsByAttributeFromExternalSchemas()
+    {
+        $tagImportParser = self::instance();
+        AbstractObjectContainer::purgeAllCache();
+
+        $tagImportParser->parse();
+        $elements = $tagImportParser->getGenerator()->getWsdl(0)->getContent()->getElementsByNameAndAttributes(WsdlDocument::TAG_ELEMENT, array(
+            'name' => 'PartnerCredentials',
+        ), true);
+
+        $this->assertNotEmpty($elements);
+        $this->assertContainsOnlyInstancesOf('\\WsdlToPhp\\PackageGenerator\\DomHandler\\Wsdl\\Tag\\TagElement', $elements);
     }
 }
