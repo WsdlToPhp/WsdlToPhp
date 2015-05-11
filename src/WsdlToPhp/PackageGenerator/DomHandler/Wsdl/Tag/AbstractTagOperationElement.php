0f3b52f88f94974dbdbbc6d425f7ae0c9b9bd3d2
Mikael DELSOL <delsol.mikael@gmail.com>
2015-05-11 17:12:16 +0200

    add getParentOperation method

--- src/WsdlToPhp/PackageGenerator/DomHandler/Wsdl/Tag/AbstractTagOperationElement.php
+++ src/WsdlToPhp/PackageGenerator/DomHandler/Wsdl/Tag/AbstractTagOperationElement.php
@@ -2,6 +2,15 @@
 
 namespace WsdlToPhp\PackageGenerator\DomHandler\Wsdl\Tag;
 
+use WsdlToPhp\PackageGenerator\DomHandler\Wsdl\Wsdl as WsdlDocument;
+
 abstract class AbstractTagOperationElement extends AbstractTag
 {
+    /**
+     * @return TagOperation|null
+     */
+    public function getParentOperation()
+    {
+        return $this->getStrictParent(WsdlDocument::TAG_OPERATION);
+    }
 }
