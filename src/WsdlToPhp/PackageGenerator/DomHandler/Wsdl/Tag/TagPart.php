443e609c5f1401fd678a7db9435a88a95694d398
Mikael DELSOL <delsol.mikael@gmail.com>
2015-05-11 14:08:32 +0200

    refactore methods and add method to get namespace

--- src/WsdlToPhp/PackageGenerator/DomHandler/Wsdl/Tag/TagPart.php
+++ src/WsdlToPhp/PackageGenerator/DomHandler/Wsdl/Tag/TagPart.php
@@ -10,18 +10,34 @@ class TagPart extends AbstractTag
         ATTRIBUTE_ELEMENT = 'element',
         ATTRIBUTE_TYPE    = 'type';
     /**
-     * @return string
+     * @param bool $returnValue
+     * @return null|string|\WsdlToPhp\PackageGenerator\DomHandler\AttributeHandler
      */
-    public function getAttributeElement()
+    public function getAttributeElement($returnValue = true)
     {
-        return $this->hasAttribute(self::ATTRIBUTE_ELEMENT) === true ? $this->getAttribute(self::ATTRIBUTE_ELEMENT)->getValue() : '';
+        if ($this->hasAttribute(self::ATTRIBUTE_ELEMENT) === true) {
+            if ($returnValue === true) {
+                return $this->getAttribute(self::ATTRIBUTE_ELEMENT)->getValue();
+            } else {
+                return $this->getAttribute(self::ATTRIBUTE_ELEMENT);
+            }
+        }
+        return $returnValue === true ? '' : null;
     }
     /**
-     * @return string
+     * @param bool $returnValue
+     * @return string|\WsdlToPhp\PackageGenerator\DomHandler\AttributeHandler
      */
-    public function getAttributeType()
+    public function getAttributeType($returnValue = true)
     {
-        return $this->hasAttribute(self::ATTRIBUTE_TYPE) === true ? $this->getAttribute(self::ATTRIBUTE_TYPE)->getValue() : '';
+        if ($this->hasAttribute(self::ATTRIBUTE_TYPE) === true) {
+            if ($returnValue === true) {
+                return $this->getAttribute(self::ATTRIBUTE_TYPE)->getValue();
+            } else {
+                return $this->getAttribute(self::ATTRIBUTE_TYPE);
+            }
+        }
+        return $returnValue === true ? '' : null;
     }
     /**
      * @return string
@@ -41,5 +57,19 @@ class TagPart extends AbstractTag
             }
         }
         return $type;
+    }
+    /**
+     * @return null|string
+     */
+    public function getFinalNamespace()
+    {
+        $attribute = $this->getAttributeType(false);
+        if (empty($attribute)) {
+            $attribute = $this->getAttributeElement(false);
+            if (empty($attribute)) {
+                return $attribute;
+            }
+        }
+        return $attribute->getNamespace();
     }
 }
