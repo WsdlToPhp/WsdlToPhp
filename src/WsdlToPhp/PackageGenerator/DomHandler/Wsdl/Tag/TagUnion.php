4810a48f8a8da220af1306f8aa2032f89d35bf5d
Mikael DELSOL <delsol.mikael@gmail.com>
2015-05-12 15:46:39 +0200

    fix class declaration, add getAttributeMemberTypes and parseMemberTypes methods

--- src/WsdlToPhp/PackageGenerator/DomHandler/Wsdl/Tag/TagUnion.php
+++ src/WsdlToPhp/PackageGenerator/DomHandler/Wsdl/Tag/TagUnion.php
@@ -2,6 +2,30 @@
 
 namespace WsdlToPhp\PackageGenerator\DomHandler\Wsdl\Tag;
 
-class TagUnique extends AbstractTag
+class TagUnion extends AbstractTag
 {
+    const
+        ATTRIBUTE_MEMBER_TYPES = 'memberTypes';
+    /**
+     * @return array[string]
+     */
+    public function getAttributeMemberTypes()
+    {
+        return $this->parseMemberTypes();
+    }
+    /**
+     * @return array[string]
+     */
+    protected function parseMemberTypes()
+    {
+        $memberTypes = array();
+        $value = $this->hasAttribute(self::ATTRIBUTE_MEMBER_TYPES) === true ? $this->getAttribute(self::ATTRIBUTE_MEMBER_TYPES)->getValue(true) : '';
+        if (!empty($value)) {
+            $values = explode(' ', $value);
+            foreach ($values as $val) {
+                $memberTypes[] = implode('', array_slice(explode(':', $val), -1, 1));
+            }
+        }
+        return $memberTypes;
+    }
 }
