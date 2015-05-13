effd4aaecb34017ccbc5f50050a3ef821e54483b
Mikael DELSOL <delsol.mikael@gmail.com>
2015-05-13 21:06:01 +0200

    move cases to CASES.md, use TagListParser

--- src/WsdlToPhp/PackageGenerator/Parser/SoapClient/AbstractParser.php
+++ src/WsdlToPhp/PackageGenerator/Parser/SoapClient/AbstractParser.php
@@ -3,7 +3,6 @@
 namespace WsdlToPhp\PackageGenerator\Parser\SoapClient;
 
 use WsdlToPhp\PackageGenerator\Parser\AbstractParser as Parser;
-use WsdlToPhp\PackageGenerator\Generator\Generator;
 
 abstract class AbstractParser extends Parser
 {
