17ce2024064a093fee7fad20daa76f53b67ab5e1
Mikael DELSOL <delsol.mikael@gmail.com>
2015-05-13 22:16:50 +0200

    remove not more usefull methods that have been gathered in the parent class

--- src/WsdlToPhp/PackageGenerator/Parser/Wsdl/TagAttribute.php
+++ src/WsdlToPhp/PackageGenerator/Parser/Wsdl/TagAttribute.php
@@ -2,80 +2,15 @@
 
 namespace WsdlToPhp\PackageGenerator\Parser\Wsdl;
 
-use WsdlToPhp\PackageGenerator\DomHandler\AbstractAttributeHandler;
 use WsdlToPhp\PackageGenerator\DomHandler\Wsdl\Wsdl as WsdlDocument;
-use WsdlToPhp\PackageGenerator\DomHandler\Wsdl\Tag\TagAttribute as Attribute;
-use WsdlToPhp\PackageGenerator\Model\Wsdl;
-use WsdlToPhp\PackageGenerator\Model\Schema;
 
-class TagAttribute extends AbstractTagParser
+class TagAttribute extends AbstractAttributesParser
 {
-    /**
-     * @see \WsdlToPhp\PackageGenerator\Parser\Wsdl\AbstractParser::getTags()
-     * @return array[\WsdlToPhp\PackageGenerator\DomHandler\Wsdl\Tag\TagAttribute]
-     */
-    public function getTags()
-    {
-        return parent::getTags();
-    }
-    /**
-     * @see \WsdlToPhp\PackageGenerator\Parser\Wsdl\AbstractParser::parseWsdl()
-     */
-    protected function parseWsdl(Wsdl $wsdl)
-    {
-        foreach ($this->getTags() as $tag) {
-            $this->parseAttribute($tag);
-        }
-    }
-    /**
-     * @see \WsdlToPhp\PackageGenerator\Parser\Wsdl\AbstractParser::parseSchema()
-     */
-    protected function parseSchema(Wsdl $wsdl, Schema $schema)
-    {
-        $this->parseWsdl($wsdl);
-    }
     /**
      * @see \WsdlToPhp\PackageGenerator\Parser\Wsdl\AbstractParser::parsingTag()
      */
     protected function parsingTag()
     {
         return WsdlDocument::TAG_ATTRIBUTE;
-    }
-    /**
-     * @param Attribute $attribute
-     */
-    public function parseAttribute(Attribute $attribute)
-    {
-        $parent = $attribute->getSuitableParent();
-        if ($parent !== null && $attribute->hasAttributeName() && ($model = $this->getModel($parent)) !== null && ($modelAttribute = $model->getAttribute($attribute->getAttributeName())) !== null) {
-            if ($modelAttribute !== null) {
-                foreach ($attribute->getAttributes() as $attributeAttribute) {
-                    switch ($attributeAttribute->getName()) {
-                        case AbstractAttributeHandler::ATTRIBUTE_NAME:
-                            /**
-                             * Avoid this attribute to be added as meta
-                             */
-                            break;
-                        case AbstractAttributeHandler::ATTRIBUTE_TYPE:
-                            $type = $attributeAttribute->getValue();
-                            if ($type !== null) {
-                                $typeModel = $this->generator->getStruct($type);
-                                $modelAttributeType = $modelAttribute->getType();
-                                if ($typeModel !== null && (empty($modelAttributeType) || strtolower($modelAttributeType) === 'unknown')) {
-                                    if ($typeModel->getIsRestriction()) {
-                                        $modelAttribute->setType($typeModel->getName());
-                                    } elseif (!$typeModel->getIsStruct() && $typeModel->getInheritance()) {
-                                        $modelAttribute->setType($typeModel->getInheritance());
-                                    }
-                                }
-                            }
-                            break;
-                        default:
-                            $modelAttribute->addMeta($attributeAttribute->getName(), $attributeAttribute->getValue());
-                            break;
-                    }
-                }
-            }
-        }
     }
 }
--- src/WsdlToPhp/PackageGenerator/Parser/Wsdl/TagElement.php
+++ src/WsdlToPhp/PackageGenerator/Parser/Wsdl/TagElement.php
@@ -2,85 +2,15 @@
 
 namespace WsdlToPhp\PackageGenerator\Parser\Wsdl;
 
-use WsdlToPhp\PackageGenerator\DomHandler\AbstractAttributeHandler;
 use WsdlToPhp\PackageGenerator\DomHandler\Wsdl\Wsdl as WsdlDocument;
-use WsdlToPhp\PackageGenerator\DomHandler\Wsdl\Tag\TagElement as Element;
-use WsdlToPhp\PackageGenerator\Model\Wsdl;
-use WsdlToPhp\PackageGenerator\Model\Schema;
 
-class TagElement extends AbstractTagParser
+class TagElement extends AbstractAttributesParser
 {
-    const
-        ATTRIBUTE_ABSTRACT = 'abstract';
-    /**
-     * @see \WsdlToPhp\PackageGenerator\Parser\Wsdl\AbstractParser::getTags()
-     * @return array[\WsdlToPhp\PackageGenerator\DomHandler\Wsdl\Tag\TagElement]
-     */
-    public function getTags()
-    {
-        return parent::getTags();
-    }
-    /**
-     * @see \WsdlToPhp\PackageGenerator\Parser\Wsdl\AbstractParser::parseWsdl()
-     */
-    protected function parseWsdl(Wsdl $wsdl)
-    {
-        foreach ($this->getTags() as $tag) {
-            $this->parseElement($tag);
-        }
-    }
-    /**
-     * @see \WsdlToPhp\PackageGenerator\Parser\Wsdl\AbstractParser::parseSchema()
-     */
-    protected function parseSchema(Wsdl $wsdl, Schema $schema)
-    {
-        $this->parseWsdl($wsdl);
-    }
     /**
      * @see \WsdlToPhp\PackageGenerator\Parser\Wsdl\AbstractParser::parsingTag()
      */
     protected function parsingTag()
     {
         return WsdlDocument::TAG_ELEMENT;
-    }
-    /**
-     * @param Element $element
-     */
-    public function parseElement(Element $element)
-    {
-        $parent = $element->getSuitableParent();
-        if ($parent !== null && $element->hasAttributeName() && ($model = $this->getModel($parent)) !== null && ($modelAttribute = $model->getAttribute($element->getAttributeName())) !== null) {
-            if ($modelAttribute !== null) {
-                foreach ($element->getAttributes() as $elementAttribute) {
-                    switch ($elementAttribute->getName()) {
-                        case AbstractAttributeHandler::ATTRIBUTE_NAME:
-                            /**
-                             * Avoid this attribute to be added as meta
-                             */
-                            break;
-                        case AbstractAttributeHandler::ATTRIBUTE_TYPE:
-                            $type = $elementAttribute->getValue();
-                            if ($type !== null) {
-                                $typeModel = $this->generator->getStruct($type);
-                                $modelAttributeType = $modelAttribute->getType();
-                                if ($typeModel !== null && (empty($modelAttributeType) || strtolower($modelAttributeType) === 'unknown')) {
-                                    if ($typeModel->getIsRestriction()) {
-                                        $modelAttribute->setType($typeModel->getName());
-                                    } elseif (!$typeModel->getIsStruct() && $typeModel->getInheritance()) {
-                                        $modelAttribute->setType($typeModel->getInheritance());
-                                    }
-                                }
-                            }
-                            break;
-                        case self::ATTRIBUTE_ABSTRACT:
-                            $model->setIsAbstract($elementAttribute->getValue(false, true, 'bool'));
-                            break;
-                        default:
-                            $modelAttribute->addMeta($elementAttribute->getName(), $elementAttribute->getValue());
-                            break;
-                    }
-                }
-            }
-        }
     }
 }
