524a6e64f8987b25efd2ba68ee0cbfe7ce7c48e4
Mikael DELSOL <delsol.mikael@gmail.com>
2015-08-06 23:52:52 +0200

    remove useless files

--- src/WsdlToPhp/PackageGenerator/frontierlinkv1.sh
+++
@@ -1,5 +0,0 @@
-php console wsdltophp:generate:package \
-    --wsdl-urlorpath="/var/www/workspace/FrontierLink/aapt-live/aapt-v1/FrontierLink.Certification.wsdl" \
-    --wsdl-destination="/var/www/workspace/FrontierLink/wsdltophp_team/FrontierLinkV1" \
-    --wsdl-prefix="FLV1" \
-    --force
